f0b9f0ed02a80f55f39d52a088cbf16852007c7e
JustBecerra <justj.becerra@gmail.com>
2022-02-07 22:42:40 -0300

    boceto de tool bar

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToolBar from '../src/components/ToolBar/ToolBar.tsx';
+
+function App() {
+  return (
+    <div>
+      <ToolBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/ToolBar/ToolBar.tsx
@@ -0,0 +1,10 @@
+import './toolBar.css'
+
+export default function ToolBar(){
+    return(
+        <div className='background'>
+            <header className='headerT'>Welcome to Spending Control</header>
+            <button className='addButton'>Add</button>
+        </div>
+    )
+}
