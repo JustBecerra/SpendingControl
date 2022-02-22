4ed00e329a4023f4ff3542683cdfd6b515fe4143
JustBecerra <justj.becerra@gmail.com>
2022-02-22 18:34:15 -0300

    ajustando el feed

--- app/src/components/ExpensesField/Fields.jsx
+++ app/src/components/ExpensesField/Fields.jsx
@@ -1,21 +1,37 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import './Fields.css'
 
 export default function Fields(){
+    let [budget,setbudget] = useState(0)
+
     let day = useSelector(state => state.Day)
-    let expenses = useSelector(state => state.Expenses).filter(ex => ex.day === day)
+    let expenses = useSelector(state => state.Expenses)
+    let TotalExps = 0;
+    for(let i=0; i < expenses.length;i++){
+        TotalExps += parseInt(expenses[i].cost);
+    }
+
+    let FilteredExps = expenses.filter(ex => ex.day === day)
     return(
         <div className='bg'>
-           <tr className='infolist'>
-               <th>type</th>
-               <th>amount</th>
-               <th>cost</th>
+           <tr className='infoTitle'>
+               <th className='categoryTitle'>type</th>
+               <th className='categoryTitle'>amount</th>
+               <th className='categoryTitle'>cost</th>
            </tr>
-           {Array.isArray(expenses) && expenses.length > 0 ? expenses.map(ex => <tr className='infolist' key={ex.cost * Math.random()}>
+           {Array.isArray(FilteredExps) && FilteredExps.length > 0 ? FilteredExps.map(ex => <tr className='infoFeed' key={ex.cost * Math.random()}>
                <td>{ex.type ? ex.type + ' ' : ''}</td>
                <td>{ex.amount ? ex.amount + ' ' : ''}</td>
                <td>{ex.cost ? ex.cost + ' ' : ''}</td>
-            </tr>) : ''}             
+            </tr>) : ''}
+            <div className='totalExps'>
+                Total Expenses:
+                ${TotalExps}<br/>
+                <input type='text' placeholder='Enter your budget'></input>
+                
+            </div>
+                       
         </div>
     )
 } 
