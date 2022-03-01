1b182bbd385cf362928f4af8421ca6a3235e8a55
JustBecerra <justj.becerra@gmail.com>
2022-03-01 12:44:05 -0300

    contenido de tablas alineado

--- app/src/components/ExpensesField/Fields.jsx
+++ app/src/components/ExpensesField/Fields.jsx
@@ -17,18 +17,24 @@ export default function Fields(){
 
     return(
         <div className='bg'>
+        <table className='tableSt'>
+          <thead>
            <tr className='infoTitle'>
                <th className='categoryTitle'>type of payment</th>
                <th className='categoryTitle'>product/service</th>
                <th className='categoryTitle'>amount</th>
                <th className='categoryTitle'>cost</th>
            </tr>
+          </thead>
+          <tbody>
            {Array.isArray(FilteredExps) && FilteredExps.length > 0 ? FilteredExps.map(ex => <tr className='infoFeed' key={ex.cost * Math.random()}>
                <td className='type'>{ex.type ? ex.type + ' ' : ''}</td>
                <td className='product'>{ex.product ? ex.product + ' ' : ''}</td>
                <td className='amount'>{ex.amount ? ex.amount + ' ' : ''}</td>
                <td className='cost'>{ex.cost ? '$' + ex.cost + ' ' : ''}</td>
             </tr>) : ''}
+          </tbody>  
+        </table>
             <div className='sideTab'>
                 <div className='totalExps'>
                     Total Expenses:<br/>
