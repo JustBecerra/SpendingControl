2e7972dad389438c90138f5c825ede3046c8b849
JustBecerra <justj.becerra@gmail.com>
2022-03-03 14:37:04 -0300

    agregue estilos al sideTab, falta logica de careful

--- app/src/components/ExpensesField/SideTab.jsx
+++ app/src/components/ExpensesField/SideTab.jsx
@@ -1,13 +1,14 @@
 import './SideTab.css'
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useAlert } from 'react-alert'
 
 export default function SideTab(props){
   let [budget,setBudget] = useState('')
   let [budgetFlag,setBudgetFlag] = useState(false);
-  let [invalid,setInvalid] = useState(false)
+  let [careful,setCareful] = useState(false)
   const inputRef = useRef('')
   const alert = useAlert()
+
   let TotalExps = 0;
   for(let i=0; i < props.expenses.length;i++){
     TotalExps += parseInt(props.expenses[i].cost);
@@ -23,21 +24,29 @@ export default function SideTab(props){
     }
   }
 
+  useEffect(() => {
+    if(TotalExps > budget){
+      setCareful(true)
+    }else{
+      setCareful(false)
+    }
+  },[TotalExps])
+
   return(
     <div className='sideTab'>
-        <div className='totalExps'>
-            Total Expenses:<br/>
+        <h2 className='totalExps'>
+            Total Expenses:
              ${TotalExps}
-        </div>   
+             {careful === true ? alert.show('budget is smaller than your expenses!') : ''}
+        </h2>   
         {budgetFlag === false ?
             <div className='budget'>
-              <input ref={inputRef} type='text' placeholder='Enter your budget' onChange={(e) => setBudget(e.target.value)}/><br/>
-              {/* {invalid === true ? <h4 className='invalidSt'>Invalid value</h4> : ''} */}
+              <input className='inputSt' ref={inputRef} type='text' placeholder='Enter your budget' onChange={(e) => setBudget(e.target.value)}/><br/>
               <button type='button' value={budget} className='budgetButton' onClick={() => changeFlag()} >Update Budget</button>
             </div>
         : <div className='budget'>
-            Budget: ${budget}<br/>
-            <button onClick={() => setBudgetFlag(false)}>Change Budget</button>
+            <h2 className='showBudget'>Budget: ${budget} </h2><br/>
+            <button className='changeBudget' onClick={() => setBudgetFlag(false)}>Change Budget</button>
         </div>}
     </div>
   )
